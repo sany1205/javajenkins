115af89029bd40521852ad6f57e35b3c78f032fe
sany1205 <sanyukta.mall@gmail.com>
2025-04-01 17:16:57 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] s) {
-        System.out.println("Hello Oracle - India Devops Session - Bangalore ");
+        System.out.println("Hello Oracle - India Devops Session - Hyderabad ");
 
         for(int i = 0; i<=10; i++) {
             System.out.println(i);
